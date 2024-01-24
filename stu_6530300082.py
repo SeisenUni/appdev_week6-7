8dce19a9906fd9076d5f11a94f7796b72ab6ecf1
Chayathon <Chayathon14639@gmail.com>
2024-01-25 04:31:03 +0700

    edit

---
+++ stu_6530300082.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://chayathon14639:GVlE571RSj3LCD1p@cluster0.76js1dj.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+
